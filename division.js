4a5442ee5f05724714bf2392930a2188d45bbf59
polkenyk <115158381+polkenyk@users.noreply.github.com>
2023-03-23 14:26:06 +0100

    Add files via upload

---
+++ division.js
@@ -0,0 +1,10 @@
+function dividir(a,b){
+        if(a==0){
+        console.log("Nose puede dividir entre 0"); 
+
+        }else{
+            return a/b
+        }
+}
+const resultado_division = dividir(4,2);
+console.log("La division de esos dos números es 2");
