5bdc9406b22f0df9c5736b19e629c24fb56bbc06
polkenyk <115158381+polkenyk@users.noreply.github.com>
2023-03-23 13:56:01 +0100

    Add files via upload

---
+++ maximo.js
@@ -0,0 +1,10 @@
+var a = 5
+var b = 9
+function maximo(a,b){
+    if(a > b){
+      return a; }else{
+        return b;
+      }
+};
+let res_mayor = maximo(a,b);
+console.log("El mayor es "+res_mayor);
